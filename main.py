d9e0b9314c9e0407c92438ea75bf412e93425369
rafael0rueda <rrrm85@gmail.com>
2025-02-06 10:22:03 -0500

    Add player

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 import constants
+from player import *
 
 def main():
     print("Starting asteroids!")
@@ -11,12 +12,27 @@ def main():
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     time = pygame.time.Clock()
     dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    
+    updateble = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateble, drawable)
+    # Player have to go below the containers
+    player = Player(x, y)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
+        updateble.update(dt)
+        
+        screen.fill((0,0,0)) 
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
+        
         # Limit the frame to 60 FPS
         dt = time.tick(60) / 1000
         
